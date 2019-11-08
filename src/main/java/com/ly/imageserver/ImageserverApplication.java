2316d6ec7e8db031859ff9506d78c571ff97b7b0
Cynaith <894324762@qq.com>
2019-11-08 22:44:28 +0800

    优化上传
    图片过大->上传500

--- src/main/java/com/ly/imageserver/ImageserverApplication.java
+++ src/main/java/com/ly/imageserver/ImageserverApplication.java
@@ -2,6 +2,16 @@ package com.ly.imageserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class ImageserverApplication {
@@ -10,4 +20,33 @@ public class ImageserverApplication {
         SpringApplication.run(ImageserverApplication.class, args);
     }
 
+
+    /**
+     * 文件上传配置
+     *
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+/*
+          springBoot 1.0+ 推荐这种写法
+            MultipartConfigFactory factory = new MultipartConfigFactory();
+            //单个文件最大
+            factory.setMaxFileSize("20480KB"); //KB,MB
+            factory.setMaxFileSize("20480KB");
+            /// 设置总上传数据总大小
+            factory.setMaxRequestSize("102400KB");
+            return factory.createMultipartConfig();
+
+*/
+        // springBoot 2.0+ 推荐这种写法，
+
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大10M,DataUnit提供5中类型B,KB,MB,GB,TB
+        factory.setMaxFileSize(DataSize.of(10, DataUnit.MEGABYTES));
+        /// 设置总上传数据总大小10M
+        factory.setMaxRequestSize(DataSize.of(100, DataUnit.MEGABYTES));
+        return factory.createMultipartConfig();
+
+    }
 }
